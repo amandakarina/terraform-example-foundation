d9eaa581173f795034ef6ed07a36b1c416836a30
Amanda Karina Lopes de Oliveira <amandak@ciandt.com>
2021-03-26 11:46:31 -0300

    Adds name of instances in outputs

--- 3-networks/modules/hierarchical_firewall_policy/outputs.tf
+++ 3-networks/modules/hierarchical_firewall_policy/outputs.tf
@@ -14,8 +14,24 @@
  * limitations under the License.
  */
 
-output "id" {
-  value = google_compute_organization_security_policy.policy.id
+output "instances_self_links" {
+  description = "List of self-links for compute instances"
+  value       = module.base_shared_gce_instance.instances_self_links
+}
+
+output "instances_names" {
+  description = "List of names for compute instances"
+  value       = [for u in module.base_shared_gce_instance.instances_details : u.name]
+}
+
+output "instances_details" {
+  description = "List of details for compute instances"
+  value       = module.base_shared_gce_instance.instances_details
+}
+
+output "available_zones" {
+  description = "List of available zones in region"
+  value       = module.base_shared_gce_instance.available_zones
 }
 
 output "project_id" {
